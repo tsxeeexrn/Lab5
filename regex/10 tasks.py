efc8354e5333f8c5ed561ff47fe5190ff514bfa4
tsxeeexrn <anideit228@gmail.com>
2025-02-19 16:14:44 +0500

    Add files via upload

---
+++ regex/10 tasks.py
@@ -0,0 +1,43 @@
+import re
+
+def match_1(s):
+    return bool(re.fullmatch(r'a*b*', s))
+
+def match_2(s):
+    return bool(re.fullmatch(r'a{1}b{2,3}', s))
+
+def find_lowercase(s):
+    return re.findall(r'[a-z]+_[a-z]+', s)
+
+def find_uppercase(s):
+    return re.findall(r'[A-Z][a-z]+', s)
+
+def match_5_finding_anything(s):
+    return bool(re.fullmatch(r'a.*b', s))
+
+def replace(s):
+    return re.sub(r'[ ,.]', ':', s)
+
+def snake_vs_camel(s):
+    return ''.join(word.capitalize() for word in s.split('_'))
+
+def split_at_uppercase(s):
+    return re.split(r'(?=[A-Z])', s)
+
+def insert_spaces_between_capitals(s):
+    return re.sub(r'(?<!^)(?=[A-Z])', ' ', s)
+
+def camel_to_snake(s):
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', s).lower()
+if __name__ == "__main__":
+    test_string = "aabb"
+    print(match_1(test_string))  
+    print(match_2(test_string))
+    print(find_lowercase("abc_def ghi_jkl"))
+    print(find_uppercase("HelloWorld TestString"))
+    print(match_5_finding_anything("axb"))
+    print(replace("Hello, world. This is a test"))
+    print(snake_vs_camel("this_is_a_test"))
+    print(split_at_uppercase("HelloWorldTest"))
+    print(insert_spaces_between_capitals("HelloWorldTest"))
+    print(camel_to_snake("HelloWorldTest"))
